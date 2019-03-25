e2c3b9887f11268b888d9cbd644de126bbdc9761
Karoly Gossler <connor@connor.hu>
2019-03-25 21:04:58 +0100

    Update docker-entrypoint.sh
    before build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -o /spigot/BuildTools.jar
 mkdir -p /spigot/build
 cd /spigot/build
+curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -o /spigot/BuildTools.jar
 java -jar /spigot/BuildTools.jar
