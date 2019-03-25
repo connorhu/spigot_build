72c822168d03724aa2246aa02564e18731ec2b23
Karoly Gossler <connor@connor.hu>
2019-03-25 21:04:27 +0100

    Update docker-entrypoint.sh
    curl command added. every build needs a fresh buildtool jar

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -o /spigot/BuildTools.jar
 mkdir -p /spigot/build
 cd /spigot/build
 java -jar /spigot/BuildTools.jar
